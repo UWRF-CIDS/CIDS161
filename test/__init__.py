62ceb38c637c38983c7741e158ecb212c708f063
Jacob <henjacob@gmail.com>
2023-08-19 14:50:05 -0500

    added yaml file to initial test

--- test/__init__.py
+++ test/__init__.py
@@ -1,16 +1,6 @@
 import check50
 
 @check50.check()
-def prints_hello():
-    """prints "hello, world\\n" """
-    from re import match
-
-    expected = "[Hh]ello, world!?\n"
-    actual = check50.run("python3 hello.py").stdout()
-    if not match(expected, actual):
-        help = None
-        if match(expected[:-1], actual):
-            help = r"did you forget a newline ('\n') at the end of your printf string?"
-        raise check50.Mismatch("hello, world\n", actual, help=help)
-
-
+def hello_world():
+    """hello world"""
+    check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
